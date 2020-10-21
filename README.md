cb58d74c0e40eb74c5ca8894c07a6db01562b2c6
Jakob Jenkov <jakob@jenkov.com>
2020-10-21 13:02:04 +0200

    First draft - only 1 example added so far

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# javafx-examples
-A large collection of JavaFX examples demonstrating basic + advanced features of JavaFX. 
+# JavaFX Examples
+This repository contains a growing collection of JavaFX examples. 
+
+The examples come from my JavaFX tutorial series: (http://tutorials.jenkov.com)[JavaFX Tutorial].
+
---
+++ src/main/java/com/jenkov/javafx/button/ButtonExample.java
@@ -0,0 +1,33 @@
+package com.jenkov.javafx.button;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ButtonExample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Button button = new Button();
+
+        button.setText("Click me!");
+
+        button.setDisable(true);
+
+        VBox vbox = new VBox(button);
+
+        Scene scene = new Scene(vbox);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+}
